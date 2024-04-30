e3eeef0373b471361861e552d8997b136434eafa
AT1927 <atmunoz1927@gmail.com>
2024-04-30 12:10:22 -0500

    Avance hasta ejercicio 2

---
+++ EjerciciosBucles/ejerciciosBucles.js
@@ -0,0 +1,19 @@
+//Presentado por: Edgar Andres Torres Muñoz
+//ENTREGA EJERCICIOS DE BUCLES - JAVA SCRIPT
+//----------------------------------------------------------------------------------------------------
+
+//Ejercicio #1:
+
+//----------------------------------------------------------------------------------------------------
+
+//Ejercicio #2:
+console.log("°*************|Ej#2. ACUMULADO DE NUMEROS|*************°");
+var acumulado = 0;
+var numero = prompt("Ingrese un numero: ");
+
+while(numero != 0){
+    acumulado += numero;
+    console.log("El acumulado de los numeros ingresados es: "+acumulado);
+}
+console.log("Proceso finalizó!")
+console.log("El acumulado de los numeros ingresados es: "+acumulado);
