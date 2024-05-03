bb90c2bba71facf7022cff009f52459a95ae5911
AT1927 <atmunoz1927@gmail.com>
2024-05-03 17:42:52 -0500

    Avance ejercicios js

--- EjerciciosBucles/ejerciciosBucles.js
+++ EjerciciosBucles/ejerciciosBucles.js
@@ -2,18 +2,164 @@
 //ENTREGA EJERCICIOS DE BUCLES - JAVA SCRIPT
 //----------------------------------------------------------------------------------------------------
 
-//Ejercicio #1:
+//Ejercicio #3:
+console.log("°**|Ej#3. ADIVINA EL NUMERO|**°");
+var numero = 0;
+let numeroGanador = 88;
+var intentos = 0;
 
-//----------------------------------------------------------------------------------------------------
+do {
+    numero = prompt("Cual es su numero: ");
+    if(numero < numeroGanador){
+        console.log("El numero ingresado esta por debajo!");
+        numero = prompt("Cual es su numero: ");
+        intentos++;
+    } else if(numero > numeroGanador){
+        console.log("El numero ingresado esta por encima!");
+        numero = prompt("Cual es su numero: ");
+        intentos++;
+    }
+    console.log("Felicitaciones! Adivinaste el numero en "+intentos+" intentos");
+    break
+} while (numero != numeroGanador);
 
-//Ejercicio #2:
-console.log("°*************|Ej#2. ACUMULADO DE NUMEROS|*************°");
-var acumulado = 0;
+//----------------------------------------------------------------------------
+//Ejercicio #4:
+console.log("°**|Ej#3. NUMEROS PRIMOS|**°");
+var numero = parseInt(prompt("Ingresa un número:"));
+// Verificar si el número es primo
+let esPrimo = true;
+
+// Un número primo solo tiene divisores hasta su mitad
+for (let i = 2; i <= numero / 2; i++) {
+    // Si el número es divisible por algún otro número que no sea 1 o él mismo, no es primo
+    if (numero % i === 0) {
+        esPrimo = false;
+        break; // Salir del bucle tan pronto como se encuentre un divisor
+    }
+}
+// Mostrar resultado:
+if (esPrimo) {
+    console.log(numero + " es un número primo.");
+} else {
+    console.log(numero + " no es un número primo.");
+}
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #5:
+console.log("°**|Ej#5. DIVISORES DE NUMEROS|**°");
 var numero = prompt("Ingrese un numero: ");
 
-while(numero != 0){
-    acumulado += numero;
-    console.log("El acumulado de los numeros ingresados es: "+acumulado);
+console.log(`Divisores de ${numero}:`);
+    for (let i = 1; i <= numero; i++) {
+        if (numero % i === 0) {
+            console.log(i);
+        }
+    }
+
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #6:
+console.log("°**|Ej#6. ELEMENTOS ARRAY|**°");    
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Recorrer el array e imprimir cada elemento en la consola
+for (let i = 0; i < array.length; i++) {
+    console.log("Elemento " + (i + 1) + ": " + array[i]);
 }
-console.log("Proceso finalizó!")
-console.log("El acumulado de los numeros ingresados es: "+acumulado);
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #7:
+console.log("°**|Ej#7. ELEMENTOS ARRAY|**°");    
+const array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Recorrer el array e imprimir cada elemento en la consola
+for (let i = 0; i < array.length; i++) {
+    i=i*i;
+    console.log("Elemento " + (i + 1) + ": " + array2[i]);
+}
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #8:
+console.log("°**|Ej#8. ARRAY DE OBJETOS|**°");
+
+const arrayFamilia = [
+    {
+        nombre: "Edgar",
+        edad: 70,
+        relacion: "Padre",
+        residencia: "San Juan de Pasto"
+    },
+    {
+        nombre: "Patricia",
+        edad: 60,
+        relacion: "Madre",
+        residencia: "Cali"
+    },
+    {
+        nombre: "Camilo",
+        edad: 27,
+        relacion: "Hermano",
+        residencia: "San Juan de Pasto"
+    },
+    {
+        nombre: "Rosana",
+        edad: 80,
+        relacion: "Abuela",
+        residencia: "Cali"
+    },
+    {
+        nombre: "Juan",
+        edad: 90,
+        relacion: "Abuelo",
+        residencia: "Cali"
+    }
+];
+
+// Recorremos el array y mostramos un mensaje de presentación para cada miembro
+arrayFamilia.forEach((miembro) => {
+    console.log(`Hola, soy ${miembro.nombre}, tengo ${miembro.edad} años y soy su${miembro.relacion}. Vivo en ${miembro.residencia}.`);
+});
+//----------------------------------------------------------------------------
+
+//Ejercicio #9:
+console.log("°**|Ej#9. ELEMENTOS ARRAY|**°");
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #10:
+console.log("°**|Ej#10. SUMATORIA PARES/IMPARES|**°");
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #11:
+console.log("°**|Ej#11. ELEMENTOS ARRAY|**°");
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #12:
+console.log("°**|Ej#12. ELEMENTOS ARRAY|**°");
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #13:
+console.log("°**|Ej#13. PIEDRA-PAPEL-TIJERA|**°");
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #14:
+console.log("°**|Ej#14. TRIANGULO|**°");
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #15:
+console.log("°**|Ej#15. TRIANGULO|**°");
+
+//----------------------------------------------------------------------------
+
+//Ejercicio #16:
+console.log("°**|Ej#16. ORDENAMIENTO BURBUJA|**°");
